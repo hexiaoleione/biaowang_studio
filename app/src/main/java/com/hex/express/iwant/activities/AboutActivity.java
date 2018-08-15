b444f30343a92f831c8b2add8e7311b786287777
何小磊 <798711147@qq.com>
2018-08-15 13:32:39 +0800

    a4

--- app/src/main/java/com/hex/express/iwant/activities/AboutActivity.java
+++ app/src/main/java/com/hex/express/iwant/activities/AboutActivity.java
@@ -7,18 +7,12 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import butterknife.Bind;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-
-import org.apache.http.Header;
 
 import com.google.gson.Gson;
 import com.hex.express.iwant.R;
@@ -28,10 +22,14 @@ import com.hex.express.iwant.constance.PreferenceConstants;
 import com.hex.express.iwant.http.AsyncHttpUtils;
 import com.hex.express.iwant.utils.AppUtils;
 import com.hex.express.iwant.utils.PreferencesUtils;
-import com.hex.express.iwant.utils.ToastUtil;
 import com.hex.express.iwant.utils.UpdateChecker;
-import com.hex.express.iwant.views.TitleBarView;
 import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.apache.http.Header;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class AboutActivity extends BaseActivity {
 	@Bind(R.id.btn_Left)
@@ -40,7 +38,6 @@ public class AboutActivity extends BaseActivity {
 	TextView version;
 	@Bind(R.id.ll_down)
 	LinearLayout ll_down;
-	
 	private VersionBean bean2;
 	private String androidApkPath;
 	private String androidVersion = "";
